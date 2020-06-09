f12f4fe8caaf7776f423b13736b6ebd47fdcc130
Xuanwo <github@xuanwo.io>
2020-06-10 03:06:59 +0800

    Save job
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- content/en-US/posts.md
+++
@@ -1,5 +0,0 @@
----
-title: All Posts
-type: page
-layout: all_posts
----
--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
+const purgeHTML = require('purgecss-from-html')
+
+const purgeCSS = require('@fullhuman/postcss-purgecss')({
   content: [
-    'layouts/**/*.html',
+    'layouts/**/*.html'
   ],
-
   whitelist: ['.highlight'],
-  whitelistPatterns: [/^[a-z]/],
-
-  defaultExtractor: (content) => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-    return broadMatches.concat(innerMatches)
-  }
+  whitelistPatterns: [/^[a-z]+$/g],
+  extractors: [
+    {
+      extractor: purgeHTML,
+      extensions: ['html']
+    }
+  ]
 })
 
 module.exports = {
@@ -22,6 +19,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    purgecss
+    purgeCSS
   ]
 }
