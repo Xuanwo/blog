3bfb0ce63cc4cbfa19eec407b26f650f323ebd4c
Xuanwo <github@xuanwo.io>
2021-03-15 10:39:15 +0800

    api: Add debug log
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- api/cors.go
+++ api/cors.go
@@ -1,11 +1,16 @@
 package api
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/rs/cors"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	cors.AllowAll().HandlerFunc(w, r)
+	c := cors.AllowAll()
+	c.Log = log.New(os.Stderr, "[cors]", log.LstdFlags)
+
+	c.HandlerFunc(w, r)
 }
