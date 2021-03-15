9f89bc08b2ee3f2964d0f130c915da8e3c0cb4f6
Xuanwo <github@xuanwo.io>
2021-03-15 12:18:47 +0800

    api/cors: Fix vercel build
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- api/cors.go
+++ api/cors.go
@@ -15,40 +15,38 @@ var (
 	GithubUrl, _ = url.Parse("https://github.com/login/oauth/access_token")
 )
 
-type transport struct{}
-
-func (tr *transport) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r = r.Clone(context.Background())
-
-	// Replace to github.com
-	r.URL.Scheme = GithubUrl.Scheme
-	r.URL.Host = GithubUrl.Host
-	r.URL.Path = GithubUrl.Path
-	r.URL.RawPath = GithubUrl.RawPath
-
-	resp, err := http.DefaultClient.Do(r)
-	if err != nil {
-		w.WriteHeader(http.StatusBadGateway)
-		return
-	}
-	defer resp.Body.Close()
-
-	for key, value := range resp.Header {
-		for _, v := range value {
-			w.Header().Add(key, v)
-		}
-	}
-	w.WriteHeader(resp.StatusCode)
-
-	_, err = io.Copy(w, resp.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusBadGateway)
-		return
-	}
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
+	proxy := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r = r.Clone(context.Background())
+
+		// Replace to github.com
+		r.URL.Scheme = GithubUrl.Scheme
+		r.URL.Host = GithubUrl.Host
+		r.URL.Path = GithubUrl.Path
+		r.URL.RawPath = GithubUrl.RawPath
+
+		resp, err := http.DefaultClient.Do(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		defer resp.Body.Close()
+
+		for key, value := range resp.Header {
+			for _, v := range value {
+				w.Header().Add(key, v)
+			}
+		}
+		w.WriteHeader(resp.StatusCode)
+
+		_, err = io.Copy(w, resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+	})
+
 	c := cors.AllowAll()
 	c.Log = log.New(os.Stderr, "[cors] ", log.LstdFlags)
-	c.Handler(&transport{}).ServeHTTP(w, r)
+	c.Handler(proxy).ServeHTTP(w, r)
 }
